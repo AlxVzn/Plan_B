70e7e8ae65979e41ebc93d80b357dd4e3d681a3d
AlxVzn <alx.vzn@gmail.com>
2018-05-31 01:13:03 +0300

    Plan_B

---
+++ src/UF.java
@@ -0,0 +1,23 @@
+/**
+ * My first JAVA!!! program
+ */
+
+
+
+
+
+public class UF {
+
+    public static void main(String[] args) {
+
+        System.out.println("  FFF    UUU  ");
+        System.out.println("|||||| |||||||");
+        System.out.println("|||||| |||||||");
+        System.out.println("|||| | | | | |");
+        System.out.println("||||         |");
+        System.out.println("     ||||||| |");
+        System.out.println("|||| ||||||| |");
+
+
+    }
+}
